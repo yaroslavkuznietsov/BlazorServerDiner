4d6380961ab6ca46588a75abab6c73d9cd7db601
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-07-17 23:00:27 +0300

    Added OrderUpdateModel in Models folder, Added EditForm into Display .razor for OrderName update for order

---
+++ BlazorServerDinerApp/Models/OrderUpdateModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerDinerApp.Models
+{
+    public class OrderUpdateModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Name for the Order")]
+        [MaxLength(20, ErrorMessage = "You need to keep the name to a max of 20 characters")]
+        [MinLength(3, ErrorMessage = "You need to keep the name to a min of 3 characters")]
+        [Required]
+        public string OrderName { get; set; }
+    }
+}
